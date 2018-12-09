17acfdcde3ed1b91870e3d0b76640ecc6ed261a7
许超 <982179530@qq.com>
2018-12-09 12:32:30 +0800

    Update CrimeFragment.java

--- Project_twelve_DateDialog/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ Project_twelve_DateDialog/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -23,6 +23,7 @@ import static android.widget.CompoundButton.*;
 
 public class CrimeFragment extends Fragment {
 
+    
     private static final String ARG_CRIME_ID = "crime_id";
     private static final String DIALOG_DATE = "DialogDate";
     private static final String DIALOG_TIME = "time";
